0d74e6abb78efe4e6d6f60f3a7282ff8736d2638
meganmfisher <meganmichellefisher@gmail.com>
2017-09-29 13:46:30 -0600

    initial commit

--- app/app.js
+++ app/app.js
@@ -3,7 +3,29 @@ angular.module('devmtnTravel', ['ui.router']).config( function ( $stateProvider,
     .state('home',{
       url:'/',
       templateUrl: "app/about/aboutTmpl.html"
-    });
+    })
+    .state('packages', {
+      url: '/packages/:country',
+      template: './app/packages/packagesTmpl.html'
+    })
+    .state('locations', {
+      url: '/locations',
+      template: './app/locations/locationsTmpl.html'
+    })
+    .state('booked', {
+      url: '/booked/:id',
+      template: './app/booked/bookedTmpl.html'
+    })
+    .state('adventures', {
+      url: '/adventures',
+      template: './app/adventures/adventuresTmpl.html'
+    })
+    .state('contact', {
+      url: '/contact',
+      template: './app/contact/contactTmpl.html'
+    })
+
+
 
   $urlRouterProvider
     .otherwise('/');
