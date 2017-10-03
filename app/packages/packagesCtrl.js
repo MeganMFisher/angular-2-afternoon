70c8f98dfdc69ad8d76a79a246025513191e28c9
meganmfisher <meganmichellefisher@gmail.com>
2017-10-03 11:36:18 -0600

    completed

--- app/packages/packagesCtrl.js
+++ app/packages/packagesCtrl.js
@@ -1,3 +1,14 @@
-angular.module('devmtnTravel').controller('packagesCtrl', ($scope) => {
-    $scope.test = 'Packages'
+angular.module('devmtnTravel').controller('packagesCtrl', ($scope, $stateParams, mainSrvc) => {
+    
+    mainSrvc.getPackagesInfo().then(response => {
+        $scope.allPackages = response.data
+
+
+        if($stateParams.country) {
+            $scope.packages = $scope.allPackages.filter(function(package) {
+                return package.country === $stateParams.country;
+            })
+        }
+    })
+
 })
