ca5bf232b82fc7111c74902ee18a21cbd468e4b7
meganmfisher <meganmichellefisher@gmail.com>
2017-10-03 09:57:15 -0600

    booked setup

--- app/booked/bookedCtrl.js
+++ app/booked/bookedCtrl.js
@@ -1,3 +1,15 @@
-angular.module('devmtnTravel').controller('bookedCtrl', ($scope) => {
-    $scope.test = 'Booked'
+angular.module('devmtnTravel').controller('bookedCtrl', ($scope, mainSrvc, $stateParams) => {
+    mainSrvc.getPackagesInfo().then(response => {
+        $scope.allPackages = response.data;
+        console.log($scope.allPackages)
+
+        if($stateParams.id) {
+            $scope.packageIndex = $scope.allPackages.findIndex(package => {
+                return package.id === parseInt($stateParams.id)
+            })
+        }
+        if($scope.packageIndex !== -1) {
+            $scope.package = $scope.allPackages[$scope.packageIndex]
+        }
+    })
 })
--- app/mainSrvc.js
+++ app/mainSrvc.js
@@ -1,3 +1,18 @@
-angular.module('devmtnTravel').service('mainSrvc', function() {
+angular.module('devmtnTravel').service('mainSrvc', function($http) {
+
+    this.getTravelInfo = () => {
+        return $http({
+            method: 'GET',
+            url: 'https://practiceapi.devmountain.com/api/travel/travel-info'
+        })
+    }
+
+    this.getPackagesInfo = () => {
+        return $http({
+            method: 'GET',
+            url: 'https://practiceapi.devmountain.com/api/travel/package-info'
+          });
+    }
+    
   
 });
